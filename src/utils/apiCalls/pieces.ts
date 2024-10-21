ad9a98312d51ceb6052d3227a31854ec2eae0cc1
aitorcas23 <aitorcas23@gmail.com>
2024-10-21 16:25:05 +0200

    Change Piece type definition

--- src/utils/apiCalls/pieces.ts
+++ src/utils/apiCalls/pieces.ts
@@ -4,7 +4,7 @@ export interface Piece {
 	id: number;
 	title: string;
 	author?: string | null;
-	arrenger?: string | null;
+	arranger?: string | null;
 	description?: string | null;
 	media?: (number | null) | MediaEntry;
 	updatedAt: string;
