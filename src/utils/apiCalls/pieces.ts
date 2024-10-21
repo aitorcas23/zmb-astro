0a23e4de736e2006e28766f0d88b2cfc3f2cd72b
aitorcas23 <aitorcas23@gmail.com>
2024-10-21 16:44:27 +0200

    Change piece description with formatted content

--- src/utils/apiCalls/pieces.ts
+++ src/utils/apiCalls/pieces.ts
@@ -5,7 +5,22 @@ export interface Piece {
 	title: string;
 	author?: string | null;
 	arranger?: string | null;
-	description?: string | null;
+	content?: {
+		root: {
+			type: string;
+			children: {
+				type: string;
+				version: number;
+				[k: string]: unknown;
+			}[];
+			direction: ("ltr" | "rtl") | null;
+			format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+			indent: number;
+			version: number;
+		};
+		[k: string]: unknown;
+	} | null;
+	content_html?: string | null;
 	media?: (number | null) | MediaEntry;
 	updatedAt: string;
 	createdAt: string;
