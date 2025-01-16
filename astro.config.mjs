a701bfd35733be3eec48df5cb4dedf2206c6d3d1
aitorcas23 <aitorcas23@gmail.com>
2025-01-16 22:50:03 +0100

    Remove astro i18n integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 import paraglide from "@inlang/paraglide-astro";
 
-import i18nRouteGen from "astro-i18n-route-gen";
-
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
-		locales: ["eu", "es"],
-		defaultLocale: "eu",
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
@@ -22,85 +13,6 @@ export default defineConfig({
 		paraglide({
 			project: "./project.inlang",
 			outdir: "./src/paraglide",
-		}),
-		i18nRouteGen({
-			enabled: false,
-			routes: {
-				"index.astro": {},
-				berriak: {
-					es: "noticias",
-					children: {
-						"index.astro": {},
-						"[id]": {
-							children: {
-								"index.astro": {},
-								"[slug].astro": {},
-							},
-						},
-					},
-				},
-				denboraldiak: {
-					es: "temporadas",
-					children: {
-						"index.astro": {},
-						"[id]": {
-							children: {
-								"index.astro": {},
-								"[slug]": {
-									children: {
-										"index.astro": {},
-										"[concertId]": {
-											children: {
-												"index.astro": {},
-												"[concertSlug].astro": {},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-				"gure-banda": {
-					es: "nuestra-banda",
-					children: {
-						"historia.astro": {},
-						"partaideak.astro": {
-							es: "miembros.astro",
-						},
-					},
-				},
-				mediateka: {
-					es: "mediateca",
-					children: {
-						"index.astro": {},
-						edukia: {
-							es: "contenido",
-							children: {
-								"index.astro": {},
-								"[id]": {
-									children: {
-										"index.astro": {},
-										"[slug].astro": {},
-									},
-								},
-							},
-						},
-						listak: {
-							es: "listas",
-							children: {
-								"index.astro": {},
-								"[id]": {
-									children: {
-										"index.astro": {},
-										"[slug].astro": {},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
 		}),
 	],
 });
